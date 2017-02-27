6d6cda65b63b31d69157804a30b5351e827cd616
OcTaGa <fabien.cano@gmail.com>
2017-02-27 11:28:47 +0100

    File_for_Walking_Dead

--- saison_1_ep2.php
+++ saison_1_ep2.php
@@ -3,6 +3,6 @@
 <html>
         <?php include_once "../episode.php" ?>
         <body>
-          <IFRAME SRC="https://openload.co/embed/RlcG3bYpBYs/" FRAMEBORDER="0" MARGINWIDTH="0" MARGINHEIGHT="0" SCROLLING="NO" allowfullscreen="true" WIDTH=750 HEIGHT=423></IFRAME>
+          <IFRAME SRC="https://openload.co/embed/qCCJPHGmXJY/" FRAMEBORDER="0" MARGINWIDTH="0" MARGINHEIGHT="0" SCROLLING="NO" allowfullscreen="true" WIDTH=750 HEIGHT=423></IFRAME>
         </body>
 </html>
--- saison_1_ep3.php
+++ saison_1_ep3.php
@@ -3,6 +3,6 @@
 <html>
         <?php include_once "../episode.php" ?>
         <body>
-          <IFRAME SRC="https://openload.co/embed/FPNKSd5sz8s/" FRAMEBORDER="0" MARGINWIDTH="0" MARGINHEIGHT="0" SCROLLING="NO" allowfullscreen="true" WIDTH=750 HEIGHT=423></IFRAME>
+          <IFRAME SRC="https://openload.co/embed/8n2UmUXL1K8/" FRAMEBORDER="0" MARGINWIDTH="0" MARGINHEIGHT="0" SCROLLING="NO" allowfullscreen="true" WIDTH=750 HEIGHT=423></IFRAME>
         </body>
 </html>
